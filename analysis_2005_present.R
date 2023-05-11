4bbae01fd0c50cc3b9d855d3aea331567466f7fe
hadao <b09701145@ntu.edu.tw>
2023-05-11 22:52:49 +0800

    Update analysis_2005_present.R

--- analysis_2005_present.R
+++ analysis_2005_present.R
@@ -1,19 +1,63 @@
 library(stats)
+# install.packages('stats')
 library(dplyr)
 library(ggplot2)
 library(tidyr)
+# install.packages("gdata")
+library(gdata)
+# install.packages("readxl")
+library(readxl)
+
+# install.packages("purrr")
+library(purrr)
 
 # read data
-df <- read.csv("D:/Personal project/IMDB/econometrics/World Happiness Report.csv")
+df_2021 <- read_excel("D:/Personal project/IMDB/econometrics/index2021_data.xls")
+df_2021 <- subset(df_2021, select = -c(WEBNAME))
+print(str(df_2021))
+df_2022 <- read_excel("D:/Personal project/IMDB/econometrics/index2022_data.xls")
+print(str(df_2022))
 
-# check columns' names
-colnames(df)
+colnames(df_2021) <- c("CountryID", "CountryName", "Region", "WorldRank", "RegionRank", "Score", 
+                  "PropertyRights", "JudicialEffectiveness", "GovernmentIntegrity", "TaxBurden", "GovSpending", 
+                  "FiscalHealth", "BusinessFreedom", "LaborFreedom", "MonetaryFreedom", "TradeFreedom", 
+                  "InvestmentFreedom", "FinancialFreedom", "TariffRate", "IncomeTaxRate", "CorporateTaxRate", 
+                  "TaxBurdenRateOfGDP", "GovExpenditureRateOfGDP", "PopulationMillions", 
+                  "GDPBillionsPPP", "GDPGrowthRate", "5YearGDPGrowthRate", "GDPperCapitaPPP", "UnemploymentRate", 
+                  "InflationRate", "FDIInflowMillions", "PublicDebtRateOfGDP")
 
-# change colnames
-colnames(df)[7] <- "score_2019"
-print(str(df))
+colnames(df_2022) <- c("CountryID", "CountryName", "Region", "WorldRank", "RegionRank", "Score", 
+                       "PropertyRights", "JudicialEffectiveness", "GovernmentIntegrity", "TaxBurden", "GovSpending", 
+                       "FiscalHealth", "BusinessFreedom", "LaborFreedom", "MonetaryFreedom", "TradeFreedom", 
+                       "InvestmentFreedom", "FinancialFreedom", "TariffRate", "IncomeTaxRate", "CorporateTaxRate", 
+                       "TaxBurdenRateOfGDP", "GovExpenditureRateOfGDP", "PopulationMillions", 
+                       "GDPBillionsPPP", "GDPGrowthRate", "5YearGDPGrowthRate", "GDPperCapitaPPP", "UnemploymentRate", 
+                       "InflationRate", "FDIInflowMillions", "PublicDebtRateOfGDP")
 
-### other
+print(str(df_2021))
+print(str(df_2022))
+
+colnames(df_2021)
+colnames(df_2022)
+
+df_2021[, -c(2,3)] <- sapply(df_2021[, -c(2,3)], as.numeric)
+print(str(df_2021))
+df_2022[, -c(2,3)] <- sapply(df_2022[, -c(2,3)], as.numeric)
+print(str(df_2022))
+
+
+df_2021$year <- 2021
+df_2022$year <- 2022
+
+# Stack the data frames on top of each other
+merged_df <- rbind(df_2021, df_2022)
+
+# Sort by CountryName
+merged_df <- merged_df[order(merged_df$CountryName), ]
+colnames(merged_df)
+
+print(str(merged_df))
+
 
 RANKS <- c('PropertyRights', 'JudicialEffectiveness', 'GovernmentIntegrity', 'TaxBurden', 
            'GovSpending', 'FiscalHealth', 'BusinessFreedom', 
@@ -21,22 +65,24 @@ RANKS <- c('PropertyRights', 'JudicialEffectiveness', 'GovernmentIntegrity', 'Ta
            'InvestmentFreedom', 'FinancialFreedom')
 
 # earlier list plus 2019 score column
-RANKS_PLUS_TOTAL <- c('score_2019', RANKS)
+RANKS_PLUS_TOTAL <- c('Score', RANKS)
 
 # columns with other statistics for each country calculated as a percentage
 PERCENTAGE_STATS <- c('TariffRate', 'IncomeTaxRate', 'CorporateTaxRate', 
                       'TaxBurdenRateOfGDP', 'GovExpenditureRateOfGDP', 'GDPGrowthRate', 
                       '5YearGDPGrowthRate', 'UnemploymentRate', 'InflationRate', 
                       'PublicDebtRateOfGDP', 'GDPperCapitaPPP')
-
+merged_df <- drop_na(merged_df)
 
 classifier <- function(item) {
-  if (item > 80) {
-    return("darkgreen")
+  if (is.na(item) || !is.numeric(item)) {
+    return('gray')
+  } else if (item > 80) {
+    return('darkgreen')
   } else if (item > 70) {
     return('limegreen')
   } else if (item > 60) {
-    return("yellow")
+    return('yellow')
   } else if (item > 50) {
     return('orange')
   } else {
@@ -45,12 +91,12 @@ classifier <- function(item) {
 }
 
 # function for making boxplots
-boxPlots <- function(df) {
-  boxplot(df, col = sapply(apply(df, 2, median), classifier), main = "World Scores by Category", xlab = "Categories", ylab = "Score", las = 2)
+boxPlots <- function(merged_df) {
+  boxplot(merged_df, col = sapply(apply(merged_df, 2, median), classifier), main = "World Scores by Category", xlab = "", ylab = "Score", las = 2)
 }
 
 # calls function
-boxPlots(df[RANKS_PLUS_TOTAL])
+boxPlots(merged_df[RANKS_PLUS_TOTAL])
 
 ### scatter plot
 scatterPlot <- function(X, Y, predictions=NULL) {
@@ -67,28 +113,28 @@ scatterPlot <- function(X, Y, predictions=NULL) {
   print(p)
 }
 
-X_total <- df[["score_2019"]]
-Y <- df[["GDPperCapitaPPP"]]
+X_total <- merged_df[["Score"]]
+Y <- merged_df[["GDPperCapitaPPP"]]
 
 scatterPlot(X_total, Y)
 
 
 ### linear regression
-Y <- sqrt(df[["GDPperCapitaPPP"]])
+Y <- sqrt(merged_df[["GDPperCapitaPPP"]])
 names(Y) <- "sqrt of GDP per Capita"
 
-fit <- lm(Y ~ WorldRank + score_2019 + PropertyRights + JudicialEffectiveness + GovernmentIntegrity
-          + TaxBurden + GovSpending + FiscalHealth + BusinessFreedom + LaborFreedom
-          + MonetaryFreedom + TradeFreedom + InvestmentFreedom + FinancialFreedom, data = df)
+# fit <- lm(Y ~ WorldRank + score_2019 + PropertyRights + JudicialEffectiveness + GovernmentIntegrity
+#           + TaxBurden + GovSpending + FiscalHealth + BusinessFreedom + LaborFreedom
+#           + MonetaryFreedom + TradeFreedom + InvestmentFreedom + FinancialFreedom, data = df)
+# 
+# summary(fit)
 
-summary(fit)
-
-df$Intercept <- 1
+merged_df$Intercept <- 1
 
 # specify the model formula
 formula <- as.formula(paste("Y ~", paste(c("Intercept", RANKS), collapse = " + "), "- 1"))
 
 # fit the linear model
-model <- lm(formula, data = df)
+model <- lm(formula, data = merged_df)
 summary(model)
 
